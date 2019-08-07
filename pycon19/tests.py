3106fb48d9a9d27a1920b3d679d4d95cc10ae3b8
choonghwi lee <choonghwi.lee@deliveryhero.co.kr>
2019-08-07 11:17:44 +0900

    Update TestCase class

--- pycon19/tests.py
+++ pycon19/tests.py
@@ -6,19 +6,35 @@ from .problems import problem_set
 
 
 class MyTest(TestCase):
-    def run(self, result=None):
-        super(MyTest, self).run(result=result)
+    def adjust_result(self, result):
+        return sorted(result)
 
     @classmethod
     def setUpClass(cls):
         cls.solver = Solution()
         cls.start = time.time()
 
-    def test_1(self):
-        for problem, solution in problem_set:
-            actual = sorted(self.solver.solve(problem))
-            self.assertEqual(actual, sorted(solution))
-
     @classmethod
     def tearDownClass(cls):
         elapsed = time.time() - cls.start
+
+
+    @classmethod
+    def patch(cls):
+        def test_factory(test):
+            def test_function(self):
+                input_, expected = test
+                actual = self.solver.solve(input_)
+
+                self.assertEqual(
+                    self.adjust_result(actual),
+                    self.adjust_result(expected)
+                )
+
+            return test_function
+
+        for i, test in enumerate(problem_set):
+            setattr(cls, 'test_{}'.format(i), test_factory(test))
+
+
+MyTest.patch()
